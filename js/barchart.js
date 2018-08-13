6618e69fd282c19470c8aff014f63cd1bbecd2c6
ykim164 <ykim164@illinois.edu>
2018-08-13 00:54:31 -0500

    overlay bar chart

--- js/barchart.js
+++ js/barchart.js
@@ -1,4 +1,5 @@
-// Create data array of values to visualize
+
+
 
 var dataObj = []
 
@@ -14,19 +15,11 @@ console.log(dataObj)
 
 // Create variable for the SVG
 var svg = d3.select("body").append("svg")
-          .attr("height","100%")
-          .attr("width","100%");
+          .attr("height","50%")
+          .attr("width","50%");
 
-svg.selectAll("rect")
-          .data(dataObj)
-          .enter().append("rect")
-              .attr("class", "bar2")
-              .attr("height", function(d, i) {return (d.potenial * 10)})
-              .attr("width","40")
-              .attr("x", function(d, i) {return (i * 60) + 25})
-              .attr("y", function(d, i) {return 400 - (d.potenial * 10)});
 // Select, append to SVG, and add attributes to rectangles for bar chart
-svg.selectAll("rect")
+svg.selectAll(".bar")
     .data(dataObj)
     .enter().append("rect")
           .attr("class", "bar")
@@ -35,7 +28,17 @@ svg.selectAll("rect")
           .attr("x", function(d, i) {return (i * 60) + 25})
           .attr("y", function(d, i) {return 400 - (d.actual * 10)});
 
-svg.selectAll("rect")
+svg.selectAll(".bar3")
+          .data(dataObj)
+          .enter().append("rect")
+              .attr("class", "bar3")
+              .attr("height", function(d, i) {return (d.latest * 10)})
+              .attr("width","40")
+              .attr("x", function(d, i) {return (i * 60) + 25})
+              .attr("y", function(d, i) {return 400 - (d.latest * 10)});
+      
+
+svg.selectAll(".bar2")
     .data(dataObj)
     .enter().append("rect")
         .attr("class", "bar2")
@@ -44,11 +47,20 @@ svg.selectAll("rect")
         .attr("x", function(d, i) {return (i * 60) + 25})
         .attr("y", function(d, i) {return 400 - (d.potenial * 10)});
 
+
 // Select, append to SVG, and add attributes to text
-svg.selectAll("text")
+svg.selectAll(".text")
     .data(dataObj)
     .enter().append("text")
     .text(function(d) {return d.actual})
            .attr("class", "text")
            .attr("x", function(d, i) {return (i * 60) + 36})
            .attr("y", function(d, i) {return 415 - (d.actual * 10)});
+// Select, append to SVG, and add attributes to text
+svg.selectAll(".text2")
+    .data(dataObj)
+    .enter().append("text2")
+    .text(function(d) {return d.potenial})
+           .attr("class", "text2")
+           .attr("x", function(d, i) {return (i * 60) + 36})
+           .attr("y", function(d, i) {return 415 - (d.potenial * 10)});
